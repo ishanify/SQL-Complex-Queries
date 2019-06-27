04601dd77024d7ed906d624155568b2339d43a98
Ishandeep Singh <ishandeep999@gmail.com>
2019-06-27 12:32:53 -0400

    Update Occupation_Pivot.sql

--- Occupation_Pivot.sql
+++ Occupation_Pivot.sql
@@ -2,16 +2,18 @@
 Approach
 1. Explore the Occupations table on the HackerRank link.
 
-2. Create a TEMP table using CASE statements; with  row numbers as a new column and Name as per Occupations
+2. Create a TEMP table using CASE statements; with  row numbers as a new column and Name as per Occupations. For better understanding check TEMP table before grouping
 
 3. To remove the NULLS Group by row numbers assigned in Step 1
 
 */
 
-
+/* ROW NUMBERS */
 SET @r1 =0, @r2=0,@r3=0,@r4=0;
+
 SELECT min(d), min(p), min(s), min(a)
 FROM(
+/* TEMP table - first column of row numbers*/
 SELECT 
 (CASE 
     WHEN OCCUPATION = 'Doctor' THEN (@r1:=@r1+1)
@@ -20,27 +22,23 @@ SELECT
     WHEN OCCUPATION = 'Actor'  THEN (@r4:=@r4+1)
     END) as rownums
 ,
-
-
-(CASE 
-    WHEN OCCUPATION = 'Doctor' THEN NAME
-    ELSE NULL
-END) as d,
-(CASE 
-    WHEN OCCUPATION = 'Professor' THEN NAME
-    ELSE NULL
-END) as p,
-(CASE 
-    WHEN OCCUPATION = 'Singer' THEN NAME
-    ELSE NULL
-END) as s,
-(CASE 
-    WHEN OCCUPATION = 'Actor' THEN NAME
-    ELSE NULL
-END) as a
+    
+/* Different CASE Statements for different columns */
+(CASE WHEN OCCUPATION = 'Doctor' THEN NAME
+    ELSE NULL END) as d,
+    
+(CASE WHEN OCCUPATION = 'Professor' THEN NAME
+    ELSE NULL END) as p,
+    
+(CASE WHEN OCCUPATION = 'Singer' THEN NAME
+    ELSE NULL END) as s,
+    
+(CASE WHEN OCCUPATION = 'Actor' THEN NAME
+    ELSE NULL END) as a
 
 FROM OCCUPATIONS
 ORDER BY Name
 ) temp
+
 GROUP BY rownums
 ;
